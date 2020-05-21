1b1c49e54d0c1b389ef205c50bbeb362d752c04d
John Asuncion <johnasuncion0304@gmail.com>
2020-05-21 15:29:27 +0800

    Added delegate as well as max health and mana to character stats

--- Assets/Scripts/Character/CharacterStats.cs
+++ Assets/Scripts/Character/CharacterStats.cs
@@ -8,15 +8,24 @@ public class CharacterStats
     public float Health { get; private set; }
     [field: SerializeField]
     public float Mana { get; private set; }
+    [field: SerializeField]
+    public float MaxHealth { get; private set; }
+    [field: SerializeField]
+    public float MaxMana { get; private set; }
     public float defense;
     public float magicDefense;
     public float attack;
     public float magicAttack;
 
+    delegate float DeductStatDelegate(CharacterStats stats);
+    delegate float AddStatDelegate(CharacterStats stats);
+
     CharacterStats(float health, float mana, float defense, float magicDefense, float attack, float magicAttack)
     {
         this.Health = health;
+        this.MaxHealth = health;
         this.Mana = mana;
+        this.MaxMana = mana;
         this.defense = defense;
         this.magicDefense = magicDefense;
         this.attack = attack;
@@ -26,7 +35,10 @@ public class CharacterStats
     CharacterStats()
     {
         this.Health = 20;
+        this.MaxHealth = Health;
         this.Mana = 15;
+        this.MaxMana = Mana;
+
         this.defense = 5;
         this.magicDefense = 9;
         this.attack = 14;
@@ -35,15 +47,47 @@ public class CharacterStats
 
     public void DeductHealth(float amount)
     {
-        Health -= amount;
-        if (Health <= 0)
-        {
-            Health = 0;
-        }
+        Health = DeductStat(stat => stat.Health, amount);
     }
 
     public void AddHealth(float amount)
     {
-        Health += amount;
+        Health = AddStat(stat => stat.Health, MaxHealth, amount);
+    }
+
+    public void DeductMana(float amount)
+    {
+        Mana = DeductStat(stat => stat.Mana, amount);
+    }
+
+    public void AddMana(float amount)
+    {
+        Mana = AddStat(stat => stat.Mana, MaxMana, amount);
+    }
+
+    float DeductStat(DeductStatDelegate deductStatDelegate, float amount)
+    {
+        float stat = deductStatDelegate(this);
+        stat -= amount;
+
+        if (stat <= 0)
+        {
+            stat = 0;
+        }
+
+        return stat;
+    }
+
+    float AddStat(AddStatDelegate addStatDelegate, float max, float amount)
+    {
+        float stat = addStatDelegate(this);
+        stat += amount;
+
+        if (stat >= max)
+        {
+            stat = max;
+        }
+
+        return stat;
     }
 }
