7e05376767ca97cca9f7c2035f3a5eafdb1a44b4
John Asuncion <johnasuncion0304@gmail.com>
2020-05-22 19:42:20 +0800

    Added ranged skills and projectiles

---
+++ Assets/Scripts/Skills/DirectionalProjectileSkill.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Directional Projectile Skill", menuName = "ScriptableObjects/Default Skills/Directional Projectile Skill")]
+public class DirectionalProjectileSkill : DirectionalSkill
+{
+    [SerializeField]
+    SkillProjectile skillProjectilePrefab = null;
+    public int arrows = 1;
+
+    public override void CastSkill(Character caster, DungeonManager dungeonManager)
+    {
+        base.CastSkill(caster, dungeonManager);
+
+        Vector2Int direction = GetSkillDirection(caster);
+        caster.StartCoroutine(Fire(caster, direction, dungeonManager));
+    }
+
+    public virtual IEnumerator Fire(Character caster, Vector2Int direction, DungeonManager dungeonManager)
+    {
+        int index = 0;
+        while (index < arrows)
+        {
+            Vector3 spawnPosition = caster.transform.position + new Vector3(direction.x, direction.y) * castRange;
+            GameObject projectile = Instantiate(skillProjectilePrefab.gameObject, spawnPosition, skillProjectilePrefab.gameObject.transform.rotation);
+            SkillProjectile skillProjectile = projectile.GetComponent<SkillProjectile>();
+            skillProjectile.Initialize(caster, this, dungeonManager.dungeonGenerator.DungeonTerrainTiles, dungeonManager.dungeonGenerator.grid);
+            
+            Grid grid = dungeonManager.dungeonGenerator.grid;
+            Vector3 target = caster.transform.position + new Vector3Int(direction.x, direction.y, 0) * (int)skillProjectile.range;
+            skillProjectile.SetNextTile(grid.WorldToCell(target));
+
+            index++;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+
+    public override void ApplySkill(Character caster, Character target)
+    {
+        base.ApplySkill(caster, target);
+        target.stats.DeductHealth((caster.stats.attack * 2) - target.stats.defense);
+    }
+}
---
+++ Assets/Scripts/Skills/Skill Projectile/SkillProjectile.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillProjectile : MonoBehaviour
+{
+    public Vector3Int currentTile;
+    public float speed = 10;
+    public float range = 4;
+
+    public delegate void OnSetNextTile();
+    public event OnSetNextTile onSetNextTile;
+
+    Character caster;
+    Skill skill;
+    DungeonGenerator.DungeonTerrainTile[,] dungeonTerrainTiles;
+    List<Character> hits;
+    protected Grid grid;
+
+    Vector3Int nextTile;
+    public Vector3Int NextTile
+    {
+        get
+        {
+            return nextTile;
+        }
+        protected set
+        {
+            nextTile = value;
+            onSetNextTile?.Invoke();
+        }
+
+    }
+
+    public void Initialize(Character caster, Skill skill, DungeonGenerator.DungeonTerrainTile[,] dungeonTerrainTiles, Grid grid)
+    {
+        this.caster = caster;
+        this.skill = skill;
+        this.dungeonTerrainTiles = dungeonTerrainTiles;
+        this.grid = grid;
+        hits = new List<Character>();
+    }
+
+    protected virtual void Update()
+    {
+        MoveToTile(NextTile, speed);
+
+        //If projectile is within the dungeon
+        if (dungeonTerrainTiles[currentTile.x, currentTile.y] != null)
+        {
+            Character target = ReturnHit();
+            if (target != null)
+            {
+                HitCharacter(caster, target);
+                Destroy(this.gameObject);
+            }
+            else if (target == null && currentTile == nextTile)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public virtual void HitCharacter(Character caster, Character target)
+    {   
+        skill.ApplySkill(caster, target);
+    }
+
+    public virtual Character ReturnHit()
+    {
+        GridEntity gridEntity = dungeonTerrainTiles[currentTile.x, currentTile.y].gridEntity;
+        if (dungeonTerrainTiles[currentTile.x, currentTile.y].gridEntity && gridEntity.GetComponent<Character>() && !gridEntity.GetComponent<PartyMember>())
+        {
+
+            return gridEntity.GetComponent<Character>();
+        }
+
+        return null;
+    }
+
+    public virtual List<Character> ReturnHits()
+    {
+        GridEntity gridEntity = dungeonTerrainTiles[currentTile.x, currentTile.y].gridEntity;
+        Character character = gridEntity.GetComponent<Character>();
+
+        if (gridEntity != null && character)
+        {
+            hits.Add(character);
+        }
+
+        return hits;
+    }
+
+    public virtual void SetNextTile(Vector3Int target)
+    {
+        NextTile = target;
+        onSetNextTile?.Invoke();
+    }
+
+    protected virtual void MoveToTile(Vector3Int nextTile, float speed)
+    {
+        float distanceToDestination = Vector2.Distance(transform.position, grid.GetCellCenterWorld(nextTile));
+
+        if (distanceToDestination > 0.1)
+        {
+            transform.position = GridMovement.MoveToTile(transform.position, nextTile, grid, speed * Time.deltaTime);
+        }
+        else
+        {
+            currentTile = nextTile;
+            transform.position = grid.GetCellCenterWorld(currentTile);
+        }
+        currentTile = grid.WorldToCell(this.transform.position);
+    }
+}
