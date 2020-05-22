bba7e46728b98818464b152f118faedfc3b53975
John Asuncion <johnasuncion0304@gmail.com>
2020-05-22 19:41:06 +0800

    Added directional skill class

--- Assets/Scripts/Skills/DirectionalDamageSkill.cs
+++ Assets/Scripts/Skills/DirectionalDamageSkill.cs
@@ -3,48 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Directional Damage Skill", menuName = "ScriptableObjects/Default Skills/Directional Damage Skill")]
-public class DirectionalDamageSkill : Skill
+public class DirectionalDamageSkill : DirectionalSkill
 {
     public int aoeRange;
-    public enum SkillDirection
-    {
-        Front,
-        Back,
-        Left,
-        Right
-    }
-    public SkillDirection skillDirection = SkillDirection.Front;
-
     public override void CastSkill(Character caster, DungeonManager dungeonManager)
     {
         base.CastSkill(caster, dungeonManager);
         List<Character> targets = new List<Character>();
 
-        Vector2Int facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Front);
+        Vector2Int facingDirection = GetSkillDirection(caster);
 
         if (facingDirection == Vector2Int.zero)
         {
             return;
         }
-
-        switch (this.skillDirection)
-        {
-            case SkillDirection.Front:
-                facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Front);
-                break;
-            case SkillDirection.Back:
-                facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Back);
-                break;
-            case SkillDirection.Left:
-                facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Left);
-                break;
-            case SkillDirection.Right:
-                facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Right);
-                break;
-            default:
-                break;
-        }
-
+        
         for (int i = -aoeRange; i <= aoeRange; i++)
         {
             Vector3 newPostion = Vector3.zero;
@@ -57,8 +30,6 @@ public class DirectionalDamageSkill : Skill
 
                 newPostion = new Vector3(caster.transform.position.x + i, caster.transform.position.y + castRange * facingDirection.y, 0);
             }
-
-            Debug.Log(newPostion);
 
             Vector3Int tile = dungeonManager.grid.WorldToCell(newPostion);
             GridEntity gridEntity = dungeonManager.dungeonGenerator.DungeonTerrainTiles[tile.x, tile.y].gridEntity;
---
+++ Assets/Scripts/Skills/DirectionalSkill.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalSkill : Skill
+{
+    public enum SkillDirection
+    {
+        Front,
+        Back,
+        Left,
+        Right
+    }
+    public SkillDirection skillDirection = SkillDirection.Front;
+
+    public virtual Vector2Int GetSkillDirection(Character caster)
+    {
+        Vector2Int facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Front);
+
+        if (facingDirection == Vector2Int.zero)
+        {
+            return Vector2Int.zero;
+        }
+
+        switch (this.skillDirection)
+        {
+            case SkillDirection.Front:
+                facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Front);
+                break;
+            case SkillDirection.Back:
+                facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Back);
+                break;
+            case SkillDirection.Left:
+                facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Left);
+                break;
+            case SkillDirection.Right:
+                facingDirection = caster.characterDirection.GetOrientation(CharacterDirection2D.Orientation.Right);
+                break;
+            default:
+                break;
+        }
+
+        return facingDirection;
+    }
+}
